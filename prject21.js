0bed73e81acb07003fb93a66a45c8ec7cfdfe4a1
karam27 <karamrafeek2001@gmail.com>
2023-02-16 15:52:42 +0200

    project 5

---
+++ prject21.js
@@ -0,0 +1,25 @@
+function Add(){
+var n1=Number(document.getElementById('n1').value) ,
+n2= Number(document.getElementById('n2').value),
+result=document.getElementById('ew').value = n1 + n2; 
+}
+function Sub(){
+var n1=Number(document.getElementById('n1').value) ,
+n2= Number(document.getElementById('n2').value),
+result=document.getElementById('es').value = n1 - n2; 
+}
+function Mulit(){
+var n1=Number(document.getElementById('n1').value) ,
+n2= Number(document.getElementById('n2').value),
+result=document.getElementById('ea').value = n1 * n2; 
+}
+function Div(){
+var n1=Number(document.getElementById('n1').value) ,
+n2= Number(document.getElementById('n2').value),
+result=document.getElementById('eq').value = n1 / n2; 
+}
+consloe.log(parseInt("10"));
+
+
+
+
